f3d884be0aac8ea5905d5823bbae5bc9b4b6f283
Christoffer Wallén <36072821+chwallen@users.noreply.github.com>
2020-04-02 17:28:19 +0200

    Get modifiable model fields based on user
    The default behaviour in Django admin is to allow modification of all
    model fields. This is not desired in our case as modifying e.g. when an
    order was created or delivered, or removing all contents of an order,
    will cause unexpected behaviour.
    
    Fields non-superusers should not be allowed to modify are therefore set
    as read-only. Any fields that it does not make sense for a non-superuser
    to set, such as setting which organisation an event belongs to (as there
    is only one option for non-superusers!), are hidden for the user.

--- backend/admin.py
+++ backend/admin.py
@@ -31,6 +31,15 @@ class EventAdmin(ForeignKeyModelAdmin):
             form.org = request.user.org
         return form
 
+    def get_fieldsets(self, request, obj=None):
+        if not request.user.is_superuser:
+            return (
+                (None, {'fields': ['name']}),
+            )
+        return (
+            (None, {'fields': ['name', 'org']}),
+        )
+
     def get_queryset(self, request):
         if not request.user.is_superuser:
             return Event.objects.filter(org=request.user.org)
@@ -47,6 +56,15 @@ class MenuItemAdmin(ForeignKeyModelAdmin):
             form.org = request.user.org
         return form
 
+    def get_fieldsets(self, request, obj=None):
+        if not request.user.is_superuser:
+            return (
+                (None, {'fields': ['item_name']}),
+            )
+        return (
+            (None, {'fields': ['item_name', 'org']}),
+        )
+
     def get_queryset(self, request):
         if not request.user.is_superuser:
             return MenuItem.objects.filter(org=request.user.org)
@@ -55,6 +73,23 @@ class MenuItemAdmin(ForeignKeyModelAdmin):
 
 @admin.register(Order)
 class OrderAdmin(ForeignKeyModelAdmin):
+    form = ModelWithOrganisationForm
+
+    def get_fieldsets(self, request, obj=None):
+        return (
+            (None, {'fields': [
+                'customer_number', 'created_timestamp', 'delivered_timestamp', 'note', 'status', 'event', 'user'
+            ]}),
+        )
+
+    def get_readonly_fields(self, request, obj=None):
+        if not request.user.is_superuser:
+            field_list = ['created_timestamp', 'delivered_timestamp', 'status']
+            if obj is not None:
+                field_list.append('user')
+            return field_list
+        return []
+
     def get_queryset(self, request):
         if not request.user.is_superuser:
             return Order.objects.filter(user__org=request.user.org)
@@ -63,6 +98,11 @@ class OrderAdmin(ForeignKeyModelAdmin):
 
 @admin.register(OrderItem)
 class OrderItemAdmin(ForeignKeyModelAdmin):
+    def get_readonly_fields(self, request, obj=None):
+        if not request.user.is_superuser and obj is not None:
+            return ['order']
+        return []
+
     def get_queryset(self, request):
         if not request.user.is_superuser:
             return OrderItem.objects.filter(menu__org=request.user.org)
@@ -90,6 +130,16 @@ class UserAdmin(BaseUserAdmin, ForeignKeyModelAdmin):
         if not request.user.is_superuser:
             form.org = request.user.org
         return form
+
+    def get_fieldsets(self, request, obj=None):
+        if not request.user.is_superuser:
+            return (
+                (None, {'fields': ['username', 'password', 'confirm_password']}),
+            )
+        return (
+            (None, {'fields': ['username', 'password', 'confirm_password', 'org']}),
+            ('Permissions', {'fields': ['is_staff', 'is_superuser', 'groups']}),
+        )
 
     def get_queryset(self, request):
         if not request.user.is_superuser:
