687ca9fbf822536f74e645c121aa524e22cc8193
Christoffer Wallén <36072821+chwallen@users.noreply.github.com>
2020-04-22 13:27:20 +0200

    Create a login page outline

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -5,12 +5,14 @@ import Bar from './Bar/Bar';
 import Header from './Header/Header';
 import Home from './Home/Home';
 import Kitchen from './Kitchen/Kitchen';
+import Login from './Login/Login';
 import Statistics from './Statistics/Statistics';
 
 function App() {
     return (
         <>
             <Header />
+            <Route path="/login" component={Login} />
             <Route exact path="/" component={Home} />
             <Route path="/bar" component={Bar} />
             <Route path="/kitchen" component={Kitchen} />
--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -1,10 +1,66 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import { Button as MuiButton, MenuItem, TextField } from '@material-ui/core';
+import './Login.scss';
 
 function Login() {
     return (
-        <div>
-            Login
-        </div>
+        <Container fluid className="d-flex flex-column justify-content-center flex-grow-1">
+            <Row>
+                <Col
+                    className="pb-5"
+                    // @ts-ignore
+                    align="center"
+                >
+                    <h1>OrdSys</h1>
+                </Col>
+            </Row>
+            <Row className="align-self-center login-form-row">
+                <Col className="w-100">
+                    <form noValidate autoComplete="off">
+                        <TextField
+                            fullWidth
+                            label="Organisation"
+                            margin="normal"
+                            select
+                            variant="outlined"
+                        >
+                            <MenuItem>Organisation 1</MenuItem>
+                            <MenuItem>Organisation 2</MenuItem>
+                            <MenuItem>Organisation 3</MenuItem>
+                        </TextField>
+                        <TextField
+                            fullWidth
+                            label="User"
+                            margin="normal"
+                            select
+                            variant="outlined"
+                        >
+                            <MenuItem>User 1</MenuItem>
+                            <MenuItem>User 2</MenuItem>
+                            <MenuItem>User 3</MenuItem>
+                        </TextField>
+                        <TextField
+                            fullWidth
+                            label="Password"
+                            margin="normal"
+                            type="password"
+                            variant="outlined"
+                        />
+                        <MuiButton
+                            className="mt-4 mb-2"
+                            color="primary"
+                            fullWidth
+                            size='large'
+                            type="submit"
+                            variant="contained"
+                        >
+                            Sign in
+                        </MuiButton>
+                    </form>
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
