5a09ec750fa25d37a11078b0d3afab9759e82e8c
Daniel Fehrm <daniel@fehrm.se>
2021-04-16 23:48:57 +0200

    use sqlite3 for tests

--- ordsys/settings/base.py
+++ ordsys/settings/base.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
-import os
+import os, sys
 # noinspection PyPackageRequirements
 from decouple import config
 
@@ -72,17 +72,26 @@ CORS_ORIGIN_ALLOW_ALL = False
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+IS_RUNNING_TEST = 'test' in sys.argv
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME', default="ordsys"),
-        'USER': config('DB_USER', default="ordsys"),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST', default="127.0.0.1"),
-        'PORT': config('DB_PORT', default=5432),
+if IS_RUNNING_TEST:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': config('DB_NAME', default="ordsys"),
+            'USER': config('DB_USER', default="ordsys"),
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST': config('DB_HOST', default="127.0.0.1"),
+            'PORT': config('DB_PORT', default=5432),
+        }
+    }
 
 
 # Password validation
