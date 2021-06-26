60d53d41d6ee21b684f8dcc6f4d1573ecf1503a4
Albin Antti <albin_antti@hotmail.com>
2021-06-26 14:52:47 +0200

    Add datetime sorting to backed
    Adds younger_than and older_than
    which allows you to filter orders
    based on ISO datetimes in the API query.

--- backend/filters.py
+++ backend/filters.py
@@ -17,22 +17,30 @@ class OrderFilter(filters.FilterSet):
     # as per https://www.w3.org/TR/NOTE-datetime
     # In short: YYYY-MM-DDThh:mm:ss.sTZD
     # For this, the javascript date function .toISOString() can be used
+    younger_than = filters.DateTimeFilter(
         field_name='created_timestamp',
-        method='get_older_than_n_hours',
-        label='Get orders older than n hours',
+        method='get_younger_than_datetime',
+        label='Get orders younger than the given datetime'
+    )
+    older_than = filters.DateTimeFilter(
+        field_name='created_timestamp',
+        method='get_older_than_datetime',
+        label='Get orders older than the given datetime'
     )
 
     def get_younger_than_n_hours(self, queryset, field_name, value):
         time_threshold = timezone.now() - timedelta(hours=int(value))
         return queryset.filter(created_timestamp__gte=time_threshold)
 
-    def get_older_than_n_hours(self, queryset, field_name, value):
-        time_threshold = timezone.now() - timedelta(hours=int(value))
-        return queryset.filter(created_timestamp__lte=time_threshold)
+    def get_younger_than_datetime(self, queryset, field_name, value):
+        return queryset.filter(created_timestamp__gte=value)
+
+    def get_older_than_datetime(self, queryset, field_name, value):
+        return queryset.filter(created_timestamp__lte=value)
 
     class Meta:
         model = Order
         fields = (
             'beverages_only', 'exclude_status',
-            'max_age', 'min_age', 'user'
+            'max_hours', 'younger_than', 'older_than', 'user'
         )
