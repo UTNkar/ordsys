f06d6afe3cf7d9854b3a870ede1a659cbfb71871
Daniel Fehrm <daniel@fehrm.se>
2021-08-12 15:43:26 +0200

    change websocket routing to admin

--- backend/routing.py
+++ backend/routing.py
@@ -3,5 +3,5 @@ from channels.routing import URLRouter
 from backend.consumers import ModelChangesConsumer
 
 websocket_urls = URLRouter([
-    re_path(r'^ws/model_changes/$', ModelChangesConsumer.as_asgi()),
+    re_path(r'^admin/ws/model_changes/$', ModelChangesConsumer.as_asgi()),
 ])
