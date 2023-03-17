2f777664aac353dbfeb87c29fc6092bde5ae2a86
Daniel Fehrm <daniel@fehrm.se>
2023-03-17 18:48:50 +0100

    change how snackbar component is rendered

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,93 +1,98 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { Stack, StyledEngineProvider, ThemeProvider } from '@mui/material';
-import { SnackbarProvider } from 'notistack';
-import Bar from './Bar';
-import Header from './Header';
-import Home from './Home';
-import Kitchen from './Kitchen';
-import Login from './Login';
-import Pickup from './Pickup';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Stack, StyledEngineProvider, ThemeProvider } from "@mui/material";
+import { SnackbarProvider } from "notistack";
+import Bar from "./Bar";
+import Header from "./Header";
+import Home from "./Home";
+import Kitchen from "./Kitchen";
+import Login from "./Login";
+import Pickup from "./Pickup";
 import RecentOrders from "./RecentOrders";
-import Statistics from './Statistics';
-import { BarRenderMode, KitchenRenderMode, OrganisationTheme } from '../@types';
+import Statistics from "./Statistics";
+import { BarRenderMode, KitchenRenderMode, OrganisationTheme } from "../@types";
 import { useUser } from "../hooks";
 import { themes } from "../utils/themes";
+import SnackbarContent from "./SnackbarContent";
 
 function App() {
-    const { user, isLoading, isUninitialized } = useUser();
-    const isAuthenticated = Boolean(user);
+  const { user, isLoading, isUninitialized } = useUser();
+  const isAuthenticated = Boolean(user);
 
-    if (isLoading || isUninitialized) {
-        return null;
+  if (isLoading || isUninitialized) {
+    return null;
+  }
+
+  function renderComponents() {
+    if (isAuthenticated) {
+      return (
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/bar"
+            element={<Bar renderMode={BarRenderMode.FULL} />}
+          />
+          <Route path="/delivery" element={<RecentOrders title="Delivery" />} />
+          <Route
+            path="/kitchen"
+            element={<Kitchen renderMode={KitchenRenderMode.FOOD} />}
+          />
+          <Route
+            path="/history"
+            element={<RecentOrders history title="Orders last hour" />}
+          />
+          <Route path="/pickup" element={<Pickup />} />
+          <Route path="/statistics" element={<Statistics />} />
+          <Route
+            path="/tap"
+            element={<Kitchen renderMode={KitchenRenderMode.BEVERAGES} />}
+          />
+          <Route
+            path="/waiter"
+            element={<Bar renderMode={BarRenderMode.WAITER} />}
+          />
+        </Routes>
+      );
+    } else {
+      return <Login />;
     }
+  }
 
-    function renderComponents() {
-        if (isAuthenticated) {
-            return (
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route
-                        path="/bar"
-                        element={<Bar renderMode={BarRenderMode.FULL} />}
-                    />
-                    <Route
-                        path="/delivery"
-                        element={<RecentOrders title="Delivery" />}
-                    />
-                    <Route
-                        path="/kitchen"
-                        element={<Kitchen renderMode={KitchenRenderMode.FOOD} />}
-                    />
-                    <Route
-                        path="/history"
-                        element={<RecentOrders history title="Orders last hour" />}
-                    />
-                    <Route path="/pickup" element={<Pickup />} />
-                    <Route path="/statistics" element={<Statistics />} />
-                    <Route
-                        path="/tap"
-                        element={<Kitchen renderMode={KitchenRenderMode.BEVERAGES} />}
-                    />
-                    <Route
-                        path="/waiter"
-                        element={<Bar renderMode={BarRenderMode.WAITER} />}
-                    />
-                </Routes>
-            );
-        } else {
-            return <Login />;
-        }
-    }
-
-    return (
-        <StyledEngineProvider injectFirst>
-            <ThemeProvider theme={themes[user?.theme || OrganisationTheme.UTN]}>
-                <SnackbarProvider
-                    maxSnack={4}
-                    anchorOrigin={{ horizontal: 'center', vertical: 'bottom' }}
-                    autoHideDuration={5000}
-                >
-                    <Header organisation={user?.theme || OrganisationTheme.UTN} />
-                    <Stack
-                        component="main"
-                        overflow="auto"
-                        sx={(theme) => ({
-                                height: "calc(100vh - 56px)",
-                                [`${theme.breakpoints.down("sm")} and (orientation: landscape)`]: {
-                                    height: "calc(100vh - 48px)",
-                                },
-                                [`${theme.breakpoints.up("sm")}`]: {
-                                    height: "calc(100vh - 64px)",
-                                },
-                        })}
-                    >
-                        {renderComponents()}
-                    </Stack>
-                </SnackbarProvider>
-            </ThemeProvider>
-        </StyledEngineProvider>
-    );
+  return (
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={themes[user?.theme || OrganisationTheme.UTN]}>
+        <SnackbarProvider
+          maxSnack={4}
+          anchorOrigin={{ horizontal: "center", vertical: "bottom" }}
+          autoHideDuration={5000}
+          Components={{
+            warning: SnackbarContent,
+            error: SnackbarContent,
+            info: SnackbarContent,
+            success: SnackbarContent,
+          }}
+        >
+          <Header organisation={user?.theme || OrganisationTheme.UTN} />
+          <Stack
+            component="main"
+            overflow="auto"
+            sx={(theme) => ({
+              height: "calc(100vh - 56px)",
+              [`${theme.breakpoints.down("sm")} and (orientation: landscape)`]:
+                {
+                  height: "calc(100vh - 48px)",
+                },
+              [`${theme.breakpoints.up("sm")}`]: {
+                height: "calc(100vh - 64px)",
+              },
+            })}
+          >
+            {renderComponents()}
+          </Stack>
+        </SnackbarProvider>
+      </ThemeProvider>
+    </StyledEngineProvider>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/hooks/useSnackbar.ts
+++ frontend/src/hooks/useSnackbar.ts
@@ -1,41 +1,27 @@
-import React, { useCallback } from "react";
+import { useCallback } from "react";
 import { useSnackbar as useSnackbarInternal } from "notistack";
-
-import SnackbarContent from "../components/SnackbarContent";
 
 import type { AlertColor } from "@mui/material";
 import type { OptionsObject, VariantType } from "notistack";
 
-type SnackbarOptions = OptionsObject & { variant: Exclude<VariantType, "default"> };
+type SnackbarOptions = OptionsObject & {
+  variant: Exclude<VariantType, "default">;
+};
 
 export function useSnackbar() {
-    const {
-        closeSnackbar,
-        enqueueSnackbar: enqueueSnackbarInternal,
-    } = useSnackbarInternal();
+  const { closeSnackbar, enqueueSnackbar: enqueueSnackbarInternal } =
+    useSnackbarInternal();
 
-    const enqueueSnackbar = useCallback(
-        (message: string, options?: SnackbarOptions) => {
-            const variant = options?.variant || "info";
-            enqueueSnackbarInternal(message, {
-                ...options,
-                variant,
-                content: (snackbarKey) => (
-                    React.createElement(
-                        SnackbarContent,
-                        {
-                            action: options?.action,
-                            closeSnackbar,
-                            message,
-                            severity: variant as AlertColor,
-                            snackbarKey,
-                        },
-                    )
-                ),
-            });
-        },
-        [closeSnackbar, enqueueSnackbarInternal],
-    );
+  const enqueueSnackbar = useCallback(
+    (message: string, options?: SnackbarOptions) => {
+      const variant = (options?.variant || "info") as AlertColor;
+      enqueueSnackbarInternal(message, {
+        ...options,
+        variant,
+      });
+    },
+    [enqueueSnackbarInternal]
+  );
 
-    return { closeSnackbar, enqueueSnackbar };
+  return { closeSnackbar, enqueueSnackbar };
 }
