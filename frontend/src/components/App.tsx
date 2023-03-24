8a5dbb09ff1de2bdaf29730d30837f9dbe21bc58
Daniel Fehrm <daniel@fehrm.se>
2023-03-24 10:21:54 +0100

    undo formatting

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -15,83 +15,83 @@ import { themes } from "../utils/themes";
 import SnackbarContent from "./SnackbarContent";
 
 function App() {
-  const { user, isLoading, isUninitialized } = useUser();
-  const isAuthenticated = Boolean(user);
+    const { user, isLoading, isUninitialized } = useUser();
+    const isAuthenticated = Boolean(user);
 
-  if (isLoading || isUninitialized) {
-    return null;
-  }
-
-  function renderComponents() {
-    if (isAuthenticated) {
-      return (
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route
-            path="/bar"
-            element={<Bar renderMode={BarRenderMode.FULL} />}
-          />
-          <Route path="/delivery" element={<RecentOrders title="Delivery" />} />
-          <Route
-            path="/kitchen"
-            element={<Kitchen renderMode={KitchenRenderMode.FOOD} />}
-          />
-          <Route
-            path="/history"
-            element={<RecentOrders history title="Orders last hour" />}
-          />
-          <Route path="/pickup" element={<Pickup />} />
-          <Route path="/statistics" element={<Statistics />} />
-          <Route
-            path="/tap"
-            element={<Kitchen renderMode={KitchenRenderMode.BEVERAGES} />}
-          />
-          <Route
-            path="/waiter"
-            element={<Bar renderMode={BarRenderMode.WAITER} />}
-          />
-        </Routes>
-      );
-    } else {
-      return <Login />;
+    if (isLoading || isUninitialized) {
+        return null;
     }
-  }
 
-  return (
-    <StyledEngineProvider injectFirst>
-      <ThemeProvider theme={themes[user?.theme || OrganisationTheme.UTN]}>
-        <SnackbarProvider
-          maxSnack={4}
-          anchorOrigin={{ horizontal: "center", vertical: "bottom" }}
-          autoHideDuration={5000}
-          Components={{
-            warning: SnackbarContent,
-            error: SnackbarContent,
-            info: SnackbarContent,
-            success: SnackbarContent,
-          }}
-        >
-          <Header organisation={user?.theme || OrganisationTheme.UTN} />
-          <Stack
-            component="main"
-            overflow="auto"
-            sx={(theme) => ({
-              height: "calc(100vh - 56px)",
-              [`${theme.breakpoints.down("sm")} and (orientation: landscape)`]:
-                {
-                  height: "calc(100vh - 48px)",
-                },
-              [`${theme.breakpoints.up("sm")}`]: {
-                height: "calc(100vh - 64px)",
-              },
-            })}
-          >
-            {renderComponents()}
-          </Stack>
-        </SnackbarProvider>
-      </ThemeProvider>
-    </StyledEngineProvider>
-  );
+    function renderComponents() {
+        if (isAuthenticated) {
+            return (
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route
+                        path="/bar"
+                        element={<Bar renderMode={BarRenderMode.FULL} />}
+                    />
+                    <Route path="/delivery" element={<RecentOrders title="Delivery" />} />
+                    <Route
+                        path="/kitchen"
+                        element={<Kitchen renderMode={KitchenRenderMode.FOOD} />}
+                    />
+                    <Route
+                        path="/history"
+                        element={<RecentOrders history title="Orders last hour" />}
+                    />
+                    <Route path="/pickup" element={<Pickup />} />
+                    <Route path="/statistics" element={<Statistics />} />
+                    <Route
+                        path="/tap"
+                        element={<Kitchen renderMode={KitchenRenderMode.BEVERAGES} />}
+                    />
+                    <Route
+                        path="/waiter"
+                        element={<Bar renderMode={BarRenderMode.WAITER} />}
+                    />
+                </Routes>
+            );
+        } else {
+            return <Login />;
+        }
+    }
+
+    return (
+        <StyledEngineProvider injectFirst>
+            <ThemeProvider theme={themes[user?.theme || OrganisationTheme.UTN]}>
+                <SnackbarProvider
+                    maxSnack={4}
+                    anchorOrigin={{ horizontal: "center", vertical: "bottom" }}
+                    autoHideDuration={5000}
+                    Components={{
+                        warning: SnackbarContent,
+                        error: SnackbarContent,
+                        info: SnackbarContent,
+                        success: SnackbarContent,
+                    }}
+                >
+                    <Header organisation={user?.theme || OrganisationTheme.UTN} />
+                    <Stack
+                        component="main"
+                        overflow="auto"
+                        sx={(theme) => ({
+                            height: "calc(100vh - 56px)",
+                            [`${theme.breakpoints.down("sm")} and (orientation: landscape)`]:
+                            {
+                                height: "calc(100vh - 48px)",
+                            },
+                            [`${theme.breakpoints.up("sm")}`]: {
+                                height: "calc(100vh - 64px)",
+                            },
+                        })}
+                    >
+                        {renderComponents()}
+                    </Stack>
+                </SnackbarProvider>
+            </ThemeProvider>
+        </StyledEngineProvider>
+    );
 }
 
 export default App;
--- frontend/src/components/SnackbarContent/SnackbarContent.tsx
+++ frontend/src/components/SnackbarContent/SnackbarContent.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Alert, IconButton, Typography } from "@mui/material";
 import {
-  CloseRounded as CloseIcon,
-  WarningAmberRounded as WarningIcon,
-  ErrorOutlineRounded as ErrorIcon,
-  CheckCircleOutlineRounded as SuccessIcon,
-  InfoOutlined as InfoIcon,
+    CloseRounded as CloseIcon,
+    WarningAmberRounded as WarningIcon,
+    ErrorOutlineRounded as ErrorIcon,
+    CheckCircleOutlineRounded as SuccessIcon,
+    InfoOutlined as InfoIcon,
 } from "@mui/icons-material";
 
 import type { AlertColor } from "@mui/material";
@@ -14,46 +14,47 @@ import { CustomContentProps, useSnackbar } from "notistack";
 const ICON_PROPS = Object.freeze({ sx: { fontSize: "2rem" } });
 
 const ICON_MAPPING: Record<AlertColor, React.ReactNode> = Object.freeze({
-  error: <ErrorIcon {...ICON_PROPS} />,
-  info: <InfoIcon {...ICON_PROPS} />,
-  success: <SuccessIcon {...ICON_PROPS} />,
-  warning: <WarningIcon {...ICON_PROPS} />,
+    error: <ErrorIcon {...ICON_PROPS} />,
+    info: <InfoIcon {...ICON_PROPS} />,
+    success: <SuccessIcon {...ICON_PROPS} />,
+    warning: <WarningIcon {...ICON_PROPS} />,
 });
 
-const SnackbarContent = React.forwardRef<HTMLDivElement, CustomContentProps>(
-  ({ action, message, variant, id }, ref) => {
+const SnackbarContent = React.forwardRef<HTMLDivElement, CustomContentProps>(({
+    action, 
+    message, 
+    variant, 
+    id
+}, ref) => {
     const { closeSnackbar } = useSnackbar();
     const _action = typeof action === "function" ? action(id) : action;
     const severity = variant as AlertColor;
     return (
-      <Alert
-        ref={ref}
-        action={
-          _action || (
-            <IconButton
-              aria-label="Close"
-              color="inherit"
-              onClick={() => closeSnackbar(id)}
-            >
-              <CloseIcon />
-            </IconButton>
-          )
-        }
-        iconMapping={ICON_MAPPING}
-        severity={severity}
-        variant="filled"
-        sx={{
-          color: (theme) => theme.palette[severity].contrastText,
-          minWidth: 300,
-          boxShadow: 24,
-        }}
-      >
-        <Typography fontSize="1.5rem" lineHeight="1.25">
-          {message}
-        </Typography>
-      </Alert>
+        <Alert
+            ref={ref}
+            action={_action || (
+                <IconButton
+                    aria-label="Close"
+                    color="inherit"
+                    onClick={() => closeSnackbar(id)}
+                >
+                    <CloseIcon />
+                </IconButton>
+            )}
+            iconMapping={ICON_MAPPING}
+            severity={severity}
+            variant="filled"
+            sx={{
+                color: (theme) => theme.palette[severity].contrastText,
+                minWidth: 300,
+                boxShadow: 24,
+            }}
+        >
+            <Typography fontSize="1.5rem" lineHeight="1.25">
+                {message}
+            </Typography>
+        </Alert>
     );
-  }
-);
+});
 
 export default SnackbarContent;
