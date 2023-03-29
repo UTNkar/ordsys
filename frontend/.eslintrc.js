ffd4f01d7e789174266675cab2f6c2cee5a81df9
Daniel Fehrm <daniel@fehrm.se>
2023-03-29 19:40:46 +0200

    add typescript eslint as parser

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,5 +1,8 @@
 module.exports = {
   extends: ["airbnb", "airbnb-typescript"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  root: true,
   parserOptions: {
     project: ["./tsconfig.json"],
   },
