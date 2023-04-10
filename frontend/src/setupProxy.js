fe50f357c9729ba5bd9c26599876696029988019
Daniel Fehrm <daniel@fehrm.se>
2023-04-10 21:03:23 +0200

    fix linting in setupproxy

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,6 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function (app) {
+module.exports = (app) => {
+  // eslint-disable-next-line max-len
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
   app.use(
     createProxyMiddleware('/admin', {
       target: 'http://localhost:8000',
