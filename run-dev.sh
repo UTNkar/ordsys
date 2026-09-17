61e279248b55dce593fb427af4a54ef55f8f6c5b
Ebin Bellini <bengans.polare@gmail.com>
2026-09-18 00:12:56 +0200

    Bump asgiref, channels
    Also remove extraneous white space in run-dev.sh

--- run-dev.sh
+++ run-dev.sh
@@ -5,16 +5,16 @@ INIT=false
 
 # parse command line arguments, check for init arg is supplied
 while true; do
-  case "$1" in 
+  case "$1" in
     init) INIT=true; shift ;;
     *) break;;
-  esac 
+  esac
 done
 
 
 docker-compose up -d
-docker-compose exec backend pip install -r dev-requirements.txt 
-docker-compose exec backend python manage.py migrate                     
+docker-compose exec backend pip install -r dev-requirements.txt
+docker-compose exec backend python manage.py migrate
 if [[ "$INIT" == true ]]; then
     # Only do this incase the uses supplies init, i.e. they want
     # to initialize a user and organisation
